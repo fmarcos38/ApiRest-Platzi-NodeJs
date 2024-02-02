240dfa048a16b8cab778f0ea3f298d28cb173f4a
fmarcos38 <fmarcos_23@hotmail.com>
2024-02-02 17:49:01 -0300

    inicio

--- index.js
+++ index.js
@@ -1,84 +1,16 @@
 const express = require('express');
+const routerApi = require('./src/routers/index');
 
 const app = express();
 const port = 3000;
 
-//declaro un array de productos provisorio
-const arrP = [
-  {
-    id: 1,
-    nombre: 'Producto 1',
-    precio: 1000,
-    idCategory: 10
-  },
-  {
-    id: 2,
-    nombre: 'Producto 2',
-    precio: 2000,
-    idCategory: 11
-  }
-]
-/*------------ENDPOINTS---------------------------------*/
-app.get('/', (req, res) => {
-  res.send('Hello mi server en port 3000!');
-});
+app.use(express.json()); // para que express entienda los formatos json
 
-app.get('/products', (req, res) => {
-  res.json([
-    {
-      id: 1,
-      nombre: 'Producto 1',
-      precio: 1000
-    },
-    {
-      id: 2,
-      nombre: 'Producto 2',
-      precio: 2000
-    }
-  ]);
-});
+//llamo a la funcion de las rutas
+routerApi(app);
 
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
 
-  const producto = arrP.find(p => p.id == id);
-
-  if(producto){
-    res.json(producto);
-  }else{
-    res.status(404).json({error: 'Producto no encontrado'});
-  }
-});
-
-//recibo parametros por query
-app.get('/paginados', (req, res) => {
-  const { limit, offset } = req.query;
-
-  if(limit && offset){
-    res.json({
-      limit,
-      offset
-    });
-  }
-
-  res.send("no hay parametros");
-});
-
-//con 2 parametros
-app.get('/categories/:idCategory/products/:idProduct', (req, res) => {
-  const { idCategory, idProduct } = req.params;
-
-  const products = arrP.find(p => p.idCategory == idCategory && p.id == idProduct);
-
-  if(products){
-    res.json(products);
-  }
-  else{
-    res.status(404).json({error: 'Producto no encontrado'});
-  }
-});
-/*------------FIN  ENDPOINTS---------------------------------*/
+//levanto el servidor
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
---
+++ src/routers/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+//me traigo las rutas
+const productsRouter = require('./products');
+
+//creo funcion para las rutas
+function routerApi(app) {
+  const router = express.Router();
+  //creo ruta global
+  app.use('/api/v1', router);
+
+  //uso las rutas
+  router.use('/products', productsRouter);
+}
+
+module.exports = routerApi;
